2367de8b93638964a0a66402604149b251345c26
Felix Mumme <felix.mumme@gmx.net>
2020-04-24 15:32:39 +0200

    Init with Swagger-UI

--- src/main/java/com/dhbwstudent/brainstormbe/BrainstormBeApplication.java
+++ src/main/java/com/dhbwstudent/brainstormbe/BrainstormBeApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BrainstormBeApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(BrainstormBeApplication.class, args);
 	}
