4d3106ab1b7ad2887d26b867576a1da62762f01e
Felix Mumme <felix.mumme@gmx.net>
2020-04-24 12:48:01 +0200

    initial commit

---
+++ src/main/java/com/dhbwstudent/brainstormbe/BrainstormBeApplication.java
@@ -0,0 +1,13 @@
+package com.dhbwstudent.brainstormbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainstormBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainstormBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhbwstudent/brainstormbe/BrainstormBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhbwstudent.brainstormbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainstormBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
