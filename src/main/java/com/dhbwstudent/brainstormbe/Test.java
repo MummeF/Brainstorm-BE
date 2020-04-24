d4549517cd0281240633a2791342f76146dfa95a
Felix Mumme <felix.mumme@gmx.net>
2020-04-24 13:22:59 +0200

    Dateien ergänzt: Dies das (Test Commit!)

--- src/main/java/com/dhbwstudent/brainstormbe/Test.java
+++ src/main/java/com/dhbwstudent/brainstormbe/Test.java
@@ -1,6 +1,12 @@
 package com.dhbwstudent.brainstormbe;
 
+
 public class Test {
+    public Test(int bla, int blub) {
+        this.bla = bla;
+        this.blub = blub;
+    }
+
     private int bla;
     public int blub;
 }
