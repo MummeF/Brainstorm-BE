84b5f4c460b2e59792ecdbffc901ef6cc7506101
Felix Mumme <felix.mumme@gmx.net>
2020-05-04 00:36:45 +0200

    cors config

--- src/main/java/com/dhbwstudent/brainstormbe/configuration/WebSecurityConfig.java
+++ src/main/java/com/dhbwstudent/brainstormbe/configuration/WebSecurityConfig.java
@@ -15,10 +15,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest()
-                .authenticated()
-                .and().cors()
-                .and().csrf().disable();
+        http
+                .cors();
+
     }
 
 
