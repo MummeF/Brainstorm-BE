58f144aa0285c127e042bc0b972beafa3ae734d9
Felix Mumme <felix.mumme@gmx.net>
2020-05-03 21:14:53 +0200

    cors config

--- src/main/java/com/dhbwstudent/brainstormbe/configuration/WebSecurityConfig.java
+++ src/main/java/com/dhbwstudent/brainstormbe/configuration/WebSecurityConfig.java
@@ -1,24 +1,32 @@
 package com.dhbwstudent.brainstormbe.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
-public class WebSecurityConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry
-                        .addMapping("/**")
-                        .allowedOrigins(
-                                "http://localhost:3000",
-                                "http://brainstorm-dhbw.herokuapps.com"
-                        );
-            }
-        };
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and().cors();
     }
+
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry
+//                        .addMapping("/**")
+//                        .allowedOrigins(
+//                                "http://localhost:3000",
+//                                "http://brainstorm-dhbw.herokuapps.com"
+//                        );
+//            }
+//        };
+//    }
 }
