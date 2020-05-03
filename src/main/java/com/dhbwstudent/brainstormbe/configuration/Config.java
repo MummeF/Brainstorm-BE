cd9dd34f56aedcfefea207e3a282232caa3074ae
Felix Mumme <felix.mumme@gmx.net>
2020-05-03 15:57:34 +0200

    heroku config

--- src/main/java/com/dhbwstudent/brainstormbe/configuration/Config.java
+++ src/main/java/com/dhbwstudent/brainstormbe/configuration/Config.java
@@ -1,8 +1,5 @@
 package com.dhbwstudent.brainstormbe.configuration;
 
-import com.dhbwstudent.brainstormbe.main.MainService;
-import com.sun.tools.javac.Main;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
