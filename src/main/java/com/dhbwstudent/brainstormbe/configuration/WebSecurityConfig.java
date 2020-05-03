81fe25308b431af2b7b42beda2f51e2dd55a7669
Felix Mumme <felix.mumme@gmx.net>
2020-05-03 23:56:09 +0200

    cors config

--- src/main/java/com/dhbwstudent/brainstormbe/configuration/WebSecurityConfig.java
+++ src/main/java/com/dhbwstudent/brainstormbe/configuration/WebSecurityConfig.java
@@ -1,11 +1,16 @@
 package com.dhbwstudent.brainstormbe.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @EnableWebSecurity
+@Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
