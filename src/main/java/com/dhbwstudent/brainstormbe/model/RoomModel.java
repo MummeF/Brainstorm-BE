5404006bc6ce03d8f8a08e2575dc09005b64643a
Felix Mumme <felix.mumme@gmx.net>
2020-05-05 00:53:18 +0200

    security config

--- src/main/java/com/dhbwstudent/brainstormbe/model/RoomModel.java
+++ src/main/java/com/dhbwstudent/brainstormbe/model/RoomModel.java
@@ -1,10 +1,14 @@
 package com.dhbwstudent.brainstormbe.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class RoomModel {
     private long id;
     private String topic;
