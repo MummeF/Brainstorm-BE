4cb5f1dd7f4f5d481254d53c86708179e187573b
Felix Mumme <felix.mumme@gmx.net>
2020-05-04 23:57:39 +0200

    security config

--- src/main/java/com/dhbwstudent/brainstormbe/api/main/MainService.java
+++ src/main/java/com/dhbwstudent/brainstormbe/api/main/MainService.java
@@ -6,38 +6,39 @@ import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 
-    @Service
-    public class MainService {
+@Service
+public class MainService {
 
-        private HashMap<Long, RoomModel> idToRoom = new HashMap<>();
+    private HashMap<Long, RoomModel> idToRoom = new HashMap<>();
 
-        public MainService() {
+    public MainService() {
 
-        }
-
-        public Long generateRandomSessionId() {
-            long roomId = (long) (Math.random() * 899999) + 100000;
-            idToRoom.put(roomId,
-                    RoomModel.builder()
-                    .id(roomId)
-                    .build());
-            return roomId;
-        }
-
-        public boolean validateRoomId(long roomId) {
-            return idToRoom.containsKey(roomId);
-        }
-
-        public RoomModel getRoom(long roomId) {
-            return idToRoom.get(roomId);
-        }
-
-        public boolean updateRoom(RoomModel roomModel) {
-            if(idToRoom.containsKey(roomModel.getId())){
-                idToRoom.remove(roomModel.getId());
-                idToRoom.put(roomModel.getId(), roomModel);
-                return true;
-            }
-            return false;
-        }
     }
+
+    public Long generateRandomSessionId() {
+        long roomId = (long) (Math.random() * 899999) + 100000;
+        idToRoom.put(roomId,
+                RoomModel.builder()
+                        .id(roomId)
+                        .topic("")
+                        .build());
+        return roomId;
+    }
+
+    public boolean validateRoomId(long roomId) {
+        return idToRoom.containsKey(roomId);
+    }
+
+    public RoomModel getRoom(long roomId) {
+        return idToRoom.get(roomId);
+    }
+
+    public boolean updateRoom(RoomModel roomModel) {
+        if (idToRoom.containsKey(roomModel.getId())) {
+            idToRoom.remove(roomModel.getId());
+            idToRoom.put(roomModel.getId(), roomModel);
+            return true;
+        }
+        return false;
+    }
+}
