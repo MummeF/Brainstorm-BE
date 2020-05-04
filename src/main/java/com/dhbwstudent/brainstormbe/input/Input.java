626da392ab6be3c8d8d898faa1d7978256a57340
MI-28-Havoc <mustafa-soput@gmx.de>
2020-05-05 00:57:37 +0200

    Dateien vergessen ;-)

---
+++ src/main/java/com/dhbwstudent/brainstormbe/input/Input.java
@@ -0,0 +1,19 @@
+package com.dhbwstudent.brainstormbe.input;
+
+public class Input {
+    private static long id = 0;
+    String content;
+    long inputId; //Eindeutige ID aller Inputs
+    public Input (String aContent) {
+        this.content = aContent;
+        inputId = id++;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public long getInputId() {
+        return inputId;
+    }
+}
