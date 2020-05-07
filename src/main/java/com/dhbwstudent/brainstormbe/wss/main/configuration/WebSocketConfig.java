5da950766033b84441b0fcb45b715bc74d467dc0
Felix Mumme <felix.mumme@gmx.net>
2020-05-07 23:54:09 +0200

    websocket config

--- src/main/java/com/dhbwstudent/brainstormbe/wss/main/configuration/WebSocketConfig.java
+++ src/main/java/com/dhbwstudent/brainstormbe/wss/main/configuration/WebSocketConfig.java
@@ -1,10 +1,20 @@
 package com.dhbwstudent.brainstormbe.wss.main.configuration;
 
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -17,9 +27,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     }
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:3000")
+        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:3000", "https://brainstorm-dhbw.herokuapp.com", "http://brainstorm-dhbw.herokuapp.com")
         .setHandshakeHandler(new HandshakeHandler());
-        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:3000")
+        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:3000", "https://brainstorm-dhbw.herokuapp.com", "http://brainstorm-dhbw.herokuapp.com")
         .setHandshakeHandler(new HandshakeHandler()).withSockJS();
     }
 }
