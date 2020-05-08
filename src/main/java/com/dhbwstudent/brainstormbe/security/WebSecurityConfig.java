8889d5d357bbb97db875b18d4f9194b6a31b406a
Felix Mumme <felix.mumme@gmx.net>
2020-05-08 12:40:51 +0200

    websocket config

--- src/main/java/com/dhbwstudent/brainstormbe/security/WebSecurityConfig.java
+++ src/main/java/com/dhbwstudent/brainstormbe/security/WebSecurityConfig.java
@@ -13,7 +13,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .anyRequest().authenticated()
+                .antMatchers("/api").authenticated()
                 .and().httpBasic()
                 .and().cors()
                 .and().csrf().disable();
